465bdc5d99d8c2e1b58095e5ed3ff835edcb945a
Angela Alves <angelavbalves@rede.ulbra.br>
2022-08-22 12:19:36 -0300

    refactor: generic function in the service

--- Sources/Data/Service/Service.swift
+++ Sources/Data/Service/Service.swift
@@ -9,27 +9,62 @@ import Foundation
 import UIKit
 
 class Service {
-    enum MovieErrorState: Swift.Error {
-        case generic
-        case movieNotFound
-        case noConnection
-    }
-    
-    static func getAllMovies(_ completion: @escaping (Result<MoviesResponse, MovieErrorState>) -> Void) {
-        if let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=f66bae459e0caf58012f1645bfb5e772") {
-            let task = URLSession.shared.dataTask(with: url) { data, _, error in
+
+    static func getMovies<T: Decodable>(endpoint: Endpoint, page: Int = 1, _ completion: @escaping (Result<T, MovieErrorState>) -> Void) {
+
+        if let url = makeUrlFrom(endpoint: endpoint) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, _ in
                 guard let data = data else { return }
-             
-                do {
-                    let moviesAPI = try JSONDecoder().decode(MoviesResponse.self, from: data)
-                    completion(.success(moviesAPI))
-//                    print(moviesAPI)
-                } catch {
-                    print(error.localizedDescription)
-                    completion(.failure(.generic))
+
+                if let response = response as? HTTPURLResponse {
+                    switch response.statusCode {
+                        case 200...299:
+                            if let obj: T = decode(data) {
+                                completion(.success(obj))
+                            } else {
+                                completion(.failure(.generic))
+                            }
+                        case 300...399:
+                            return
+                        case 400...499:
+                            completion(.failure(.badRequest))
+                            return
+                        case 500...599:
+                            return
+                        default:
+                            return
+                    }
                 }
             }
             task.resume()
+        } else {
+            completion(.failure(.generic))
         }
+    }
+
+    private static func makeUrlFrom(endpoint: Endpoint) -> URLRequest? {
+        guard let url = buildUrlFrom(endpoint: endpoint) else { return nil }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = endpoint.method
+        return urlRequest
+    }
+
+    private static func buildUrlFrom(endpoint: Endpoint) -> URL? {
+        var component = URLComponents()
+        component.scheme = "https"
+        component.host = endpoint.host
+        component.path = endpoint.path
+        component.query = endpoint.token
+        return component.url
+    }
+
+    private static func decode<T: Decodable>(_ data: Data) -> T? {
+        do {
+            let moviesAPI = try JSONDecoder().decode(T.self, from: data)
+            return moviesAPI
+        } catch {
+            print(error)
+        }
+        return nil
     }
 }
